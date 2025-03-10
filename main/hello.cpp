ed3bc05c0fdf60fcb8b0e74f8a513dabfa13e9c2
Kartik <karthiknaik435@gmail.com>
2025-03-10 16:27:54 +0530

    edit

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void)
 {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "I have successfully  \n";
   cout << "Amazing\n";
   return 0;
 }
